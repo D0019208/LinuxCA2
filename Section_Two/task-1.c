341af5131ecddb68e048455775955c99c6cf06d0
d00192082 <d00192082@student.dkit.ie>
2020-04-25 22:16:54 +0000

    Dynamically extract data from text files in Section 2
    For Section 2 before, I used to hardcode the number of times we loop to get values from stdin, now, I use a different approach to ensure that any number of colours can work with our code

--- Section_Two/task-1.c
+++ Section_Two/task-1.c
@@ -1,30 +1,29 @@
-#define VALID_OPTIONS 25
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-	char colours[VALID_OPTIONS][20];
-	char colour[11];
+	int i = 0;
+	char **colours = malloc(1000);
+	char *colour = malloc(11 * sizeof(char));
 
-	//We loop 25 times, (the ammount of correct colour codes)
-	for(int i = 0; i < VALID_OPTIONS; i++) {
-		//Withdraw the values from stdin
-		fgets(colour, 11, stdin);
-
+	//Loop through stdin until all entries have been exhausted
+	while(fgets(colour, 11, stdin) != NULL) {
 		//Remove the \n at the end of the string literal so we can append 'ff'
 		strtok(colour, "\n");
 
 		//If the string legth is 7 (counting '\0'), add placeholder alpha value
 		if(strlen(colour) == 7) {
+			colours[i] = malloc(11 * sizeof(char));
 			strcpy(colours[i], colour);
 			strcat(colours[i], "ff");
 		} else {
+			colours[i] = malloc(11 * sizeof(char));
 			strcpy(colours[i], colour);
 		}
 
 		printf("%s\n", colours[i]);
+		i++;
 	}
 
 	return 0;
